72ea289c18b30a6f3924036cf7dd1915b3a865cd
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-03 14:38:15 -0500

    working on setp 1

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state= {
+      user1: {},
+      followers1: []
+    };
+  }
+
+  render(){
+  return (
+    <div className="App">
+     
+      
+    </div>
+  );
+  }
+}
+
+export default App;
